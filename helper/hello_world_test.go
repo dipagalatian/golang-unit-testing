f1f185f35d48541b70cf07d8586b40c5fa36fbde
dipagalatian <dipagalatian3003@gmail.com>
2025-11-26 08:08:55 +0700

    FEAT: Implement t.Fail() and t.FailNow() to replace panic() in testing file

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // Unit test for HelloWorld function
 // For testing, we can run golang command:
@@ -10,21 +13,34 @@ import "testing"
 // - go test -run TestHelloWorldAdmin (to run specific test function)
 // - go test -cover (to see code coverage)
 
-func TestHelloWorld(t *testing.T) {
+// use *testing.T struct for mark test as failed
+// use t.Fail() -> mark teset as failed but continue running rest code in the same test function
+// use t.FailNow() -> mark test as failed and stop running rest code in the same test function
+// use t.Errorf("format", args...) -> mark test as failed with formatted error message but continue running rest code in the same test function
+// use t.Fatalf("format", args...) -> mark test as failed with formatted error message and stop running rest code in the same test function
+
+func TestHelloWorldUser(t *testing.T) {
 	result := HelloWorld("Dipa")
 
 	if result != "Hello Dipa!" {
 		// Errror test
-		t.Errorf("Expected 'Hello Dipa!' but got '%s'", result)
-		panic("Test HelloWorld failed. Test did not return 'Hello Dipa!'")
+		// t.Errorf("Expected 'Hello Dipa!' but got '%s'", result)
+		// panic("Test HelloWorld failed. Test did not return 'Hello Dipa!'") -> this not recommended in testing, because it will creash/stop the all test process
+		// instead use t.Fail() to mark the test as failed but continue running the rest code in the same test function
+		t.Fail()
 	}
+	// this line will be executed even if the test fails above
+	fmt.Println("TestHelloWorldUser done")
 }
 
 func TestHelloWorldAdmin(t *testing.T) {
 	result := HelloWorld("Admin")
 
 	if result != "Hello Admin!" {
-		t.Errorf("Expected 'Hello Admin!' but got '%s'", result)
-		panic("Test HelloWorld failed. Test did not return 'Hello Admin!'")
+		// t.Errorf("Expected 'Hello Admin!' but got '%s'", result)
+		// panic("Test HelloWorld failed. Test did not return 'Hello Admin!'")
+		t.FailNow() // this will stop the test process immediately
 	}
+	// this line will not be executed if the test fails above
+	fmt.Println("TestHelloWorldAdmin done")
 }
